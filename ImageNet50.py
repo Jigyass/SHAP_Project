0ebf79373bb87e48a0d95a243dffea84ab56abff
Jigyass <jigyassharma08@gmail.com>
2024-02-03 22:42:28 -0600

    added shap outputs with predictions

--- ImageNet50.py
+++ ImageNet50.py
@@ -1,13 +1,13 @@
 import json
-import os
 import numpy as np
 import torch
-from torchvision import models
+from torchvision import models, transforms
 import shap
 import matplotlib.pyplot as plt
+from torch.nn.functional import softmax
 
-# Normalization function
-def normalize(image):
+# Normalization function remains the same
+def normalize(image, mean, std):
     if image.max() > 1:
         image /= 255
     image = (image - mean) / std
@@ -19,7 +19,6 @@ model = models.vgg16(pretrained=True).eval()
 # SHAP dataset
 X, y = shap.datasets.imagenet50()
 X /= 255
-to_explain = X[[9, 41]]
 
 # ImageNet class names
 url = "https://s3.amazonaws.com/deep-learning-models/image-models/imagenet_class_index.json"
@@ -28,16 +27,32 @@ with open(fname) as f:
     class_names = json.load(f)
 
 # SHAP Explainer
-mean = [0.485, 0.456, 0.406]
-std = [0.229, 0.224, 0.225]
-e = shap.GradientExplainer((model, model.features[7]), normalize(X))
-shap_values, indexes = e.shap_values(normalize(to_explain), ranked_outputs=2, nsamples=200)
+mean = np.array([0.485, 0.456, 0.406])
+std = np.array([0.229, 0.224, 0.225])
+e = shap.GradientExplainer((model, model.features[7]), normalize(X, mean, std))
 
-# Convert indexes to names
-index_names = np.vectorize(lambda x: class_names[str(x)][1])(indexes)
+for i in range(len(X)):
+    print(f'Processing image {i+1}/{len(X)}...')
+    to_explain = X[[9]]
+    normalized_image = normalize(to_explain, mean, std)
+    shap_values, indexes = e.shap_values(normalized_image, ranked_outputs=2, nsamples=200)
 
-# Adjust SHAP values for plotting
-shap_values = [np.swapaxes(np.swapaxes(s, 2, 3), 1, -1) for s in shap_values]
+    # Convert indexes to names
+    index_names = np.vectorize(lambda x: class_names[str(x)][1])(indexes)
 
-shap.image_plot(shap_values, to_explain, index_names)
+    # Model prediction for the top classes
+    preds = model(normalized_image)
+    probs = softmax(preds, dim=1)
+    top_probs, top_classes = torch.topk(probs, 2)  # Getting the top two predictions
+    predicted_percentage = top_probs.detach().numpy()[0] * 100  # Convert to percentage
+
+    # Adjust SHAP values for plotting
+    shap_values = [np.swapaxes(np.swapaxes(s, 2, 3), 1, -1) for s in shap_values]
+
+    # Print the top two predictions along with their percentages
+    for j in range(2):
+        print(f"Prediction {j+1}: {class_names[str(top_classes[0][j].item())][1]}, {predicted_percentage[j]:.2f}%")
+
+    # Plot the SHAP values for the current image
+    shap.image_plot(shap_values, to_explain, index_names)
 
